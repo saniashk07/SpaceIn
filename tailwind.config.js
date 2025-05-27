070c3b23bb73cf2b259c7c11563ef9af17fea2bd
Saniya Shaikh <123464381+saniashk07@users.noreply.github.com>
2025-05-27 13:07:30 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './css/**/*.{html,js,jsx,ts,tsx,vue}',
+    './public/index.html',],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
